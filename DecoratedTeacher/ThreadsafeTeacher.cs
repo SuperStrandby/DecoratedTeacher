9619b9fafccde181b14eb95a6bee5f85b2934c31
SuperStrandby <strandby@live.dk>
2014-12-01 02:21:03 +0100

    threadsafeTeacher added and unittested

---
+++ DecoratedTeacher/ThreadsafeTeacher.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratedTeacher
+{
+    public class ThreadsafeTeacher : ITeacher
+    {
+        private readonly ITeacher _teacher;
+
+        public ThreadsafeTeacher(ITeacher teacher)
+        {
+            if (teacher == null) throw  new ArgumentNullException("teacher");
+            _teacher = teacher;
+        }
+
+        public string Name
+        {
+            get
+            {
+                lock (this)
+                {
+                    return _teacher.Name;
+                }
+            }
+            set
+            {
+                lock (this)
+                {
+                    _teacher.Name = value;
+                }
+            }
+        }
+
+        public int Salary
+        {
+            get
+            {
+                lock (this)
+                {
+                    return _teacher.Salary;
+                }
+            }
+            set
+            {
+                lock (this)
+                {
+                    _teacher.Salary = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            lock (this)
+            {
+                return _teacher.ToString();
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            lock (this)
+            {
+                return _teacher.Equals(obj);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            lock (this)
+            {
+                return _teacher.GetHashCode();
+            }
+        }
+    }
+}
---
+++ DecoratedTeacherTest/ThreadsafeTeacherTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using DecoratedTeacher;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DecoratedTeacherTest
+{
+    [TestClass]
+    public class ThreadsafeTeacherTests
+    {
+        [TestMethod]
+        public void TestAll()
+        {
+            ITeacher normalTeacher = new Teacher
+            {
+                Name = "Per",
+                Salary = 48000
+            };
+            ITeacher safeTeacher = new ThreadsafeTeacher(normalTeacher);
+            Assert.AreEqual("Per", safeTeacher.Name);
+            Assert.AreEqual(48000, safeTeacher.Salary);
+            safeTeacher.Name = "John";
+            Assert.AreEqual("John", safeTeacher.Name);
+            Assert.AreEqual("John", normalTeacher.Name);
+
+            Assert.AreEqual(48000, safeTeacher.Salary);
+            safeTeacher.Salary = 36000;
+            Assert.AreEqual(36000, safeTeacher.Salary);
+            Assert.AreEqual(36000, normalTeacher.Salary);
+
+            try
+            {
+                new ThreadsafeTeacher(null);
+                Assert.Fail();
+            }
+            catch (ArgumentNullException)
+            {
+
+                /* empty */
+            }
+        }
+    }
+}
